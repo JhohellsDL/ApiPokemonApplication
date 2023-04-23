a1ca9e5bcfa5dadc6f778e2eb1c0c91287c2cab0
JhohellsDL <jhohellserick@gmail.com>
2023-04-23 00:32:51 -0500

    data, domain y injection dependencies

--- app/src/main/java/com/jdlstudios/apipokemonapplication/data/local/converters/Converters.kt
+++ app/src/main/java/com/jdlstudios/apipokemonapplication/data/local/converters/Converters.kt
@@ -4,20 +4,8 @@ import androidx.room.TypeConverter
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.jdlstudios.apipokemonapplication.data.local.entities.PokemonCacheEntity
-import com.jdlstudios.apipokemonapplication.data.repositories.models.Pokemon
 
 class Converters {
-
-    /*@TypeConverter
-    fun fromJson(value: String): List<Pokemon> {
-        val type = object:TypeToken<List<Pokemon>>() {}.type
-        return Gson().fromJson(value, type)
-    }
-
-    @TypeConverter
-    fun toJson(list: List<Pokemon>): String {
-        return Gson().toJson(list)
-    }*/
 
     @TypeConverter
     fun fromPokemonList(value: List<PokemonCacheEntity>): String {
