fea7816c95dc9f4e4e9ee3f4e28c86c64c6a2fc3
JhohellsDL <jhohellserick@gmail.com>
2023-04-23 00:32:16 -0500

    data, domain y injection dependencies

--- app/src/main/java/com/jdlstudios/apipokemonapplication/data/local/entities/PokemonListCacheEntity.kt
+++ app/src/main/java/com/jdlstudios/apipokemonapplication/data/local/entities/PokemonListCacheEntity.kt
@@ -1,11 +1,8 @@
 package com.jdlstudios.apipokemonapplication.data.local.entities
 
-import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import androidx.room.TypeConverters
-import com.jdlstudios.apipokemonapplication.data.local.converters.PokemonListConverter
-import com.jdlstudios.apipokemonapplication.data.repositories.models.Pokemon
 
 @Entity(tableName = "pokemon_list")
 data class PokemonListCacheEntity(
@@ -14,5 +11,5 @@ data class PokemonListCacheEntity(
     val previous: String?,
     val next: String?,
     @TypeConverters()
-    val results: List<Pokemon> = mutableListOf()
+    val results: List<PokemonCacheEntity> = mutableListOf()
 )
